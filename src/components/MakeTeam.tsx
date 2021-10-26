828128bee150163899d883784236567f33c7be02
kwangsun <eyeyel@naver.com>
2021-10-27 00:00:53 +0900

    팀 만들기 컴포넌트 추가

---
+++ src/components/MakeTeam.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+/** @jsxImportSource @emotion/react */ 
+import {css} from '@emotion/react';
+
+interface Props {
+}
+
+function MakeTeam({}: Props){
+    return (
+        <div css={style}>
+        </div>
+    );
+}
+
+const style = css`
+`;
+
+export default MakeTeam;
