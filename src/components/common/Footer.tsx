86c9ce7348c4b5ab209b380f31de926279c9782e
Hyejeong Kang <ovokhj@gmail.com>
2021-10-31 10:23:58 +0900

    create header, footer
    create header, footer

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+const Footer = () => {
+    return (
+        <footer css={styles}>
+            <div className="footer">
+                <div className="footer__logo">
+                    다락방
+                </div>
+                <div className="footer__info">
+                    <div className="footer__info__term">
+                        <div className="gray-16">이용 약관</div>
+                        <div className="gray-16">개인정보 처리방침</div>
+                    </div>
+                    <div className="gray-16">
+                        다락방 | 서울특별시 강남구 신사동 1234 | 전화번호 : 010-5060-8349 | uyu6610@gmail.com
+                    </div>
+                </div>
+                <div className="footer__nav">
+                    <ul>
+                        <li className="gray-16">문의하기</li>
+                        <li className="gray-16">공지사항</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+const styles = css`
+    width: 100%;
+    height: 117px;
+    
+    border-top: 1px solid #000;
+    position: fixed;
+    bottom: 0;
+
+    .footer {
+        max-width: 1285px;
+        width: 90%;
+        height: 100%;
+
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin: 0 auto;
+
+        &__logo {
+            font-size: 56px;
+            font-weight: bold;
+            color: #9D9D9D;
+        }
+
+        &__info {
+            &__term {
+                display: flex;
+
+               >  div + div {
+                    margin-left: 24px;
+               }
+            }
+
+            > div + div {
+                margin-top: 12px;
+            }
+        }
+
+        &__nav {
+            > ul {
+                list-style: none;
+
+                li + li {
+                    margin-top: 12px;
+                }
+            }
+        }
+    }
+
+    .gray-16 {
+        font-size: 16px;
+        color: #B4B4B4;
+    }
+`;
+
+export default Footer;
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+const Header = () => {
+    return (
+        <header css={styles}>
+            <div className="header">
+                <div className="header__logo">
+                    <div className="header__logo__logo">다락방</div>
+                    <div className="header__logo__about gray-16">About</div>
+                </div>
+                <div className="header__gethering">
+                    <div className="header__gethering__button">게더링</div>
+                </div>
+                <div className="header__nav">
+                    <ul>
+                        <li className="gray-16">알림</li>
+                        <li className="gray-16">로그인</li>
+                        <li className="gray-16">프로필</li>
+                        <li className="gray-16">Contact</li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+const styles = css`
+    width: 100%;
+    height: 128px;
+    border-bottom: 1px solid #C4C4C4;
+
+    .header {
+        max-width: 1285px;
+        width: 90%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin: 0 auto;
+    }
+
+    .header__logo {
+        display: flex;
+        align-items: center;
+
+        &__logo {
+            font-size: 24px;
+            font-weight: bold;
+            color: #000;
+        }
+
+        > div + div {
+            margin-left: 40px;
+        }
+    }
+
+    .header__gethering {
+        &__button {
+            padding: 8px 10px;
+            background-color: #000;
+            color: #FFF;
+            font-size: 16px;
+            font-weight: bold;
+        }
+    }
+
+    .header__nav {
+        > ul {
+            display: flex;
+            list-style: none;
+
+            li {
+                margin-right: 16px;
+            }
+            li:first-child {
+                margin-right: 40px;
+            }
+            li:last-child {
+                margin-right: 0;
+            }
+        }
+    }
+
+    .gray-16 {
+        font-size: 16px;
+        color: #B4B4B4;
+    }
+`;
+
+export default Header;
