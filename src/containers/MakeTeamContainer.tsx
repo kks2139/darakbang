76ca9fcaad9f257816f8cdd19fc4791a70e70c64
kwangsun <eyeyel@naver.com>
2021-11-01 23:45:52 +0900

    팀만들기 수정
    1. 스타일 수정
    2. 인터페이스 추가
    3. Combobox 수정

--- src/containers/MakeTeamContainer.tsx
+++ src/containers/MakeTeamContainer.tsx
@@ -1,13 +1,54 @@
 import React from "react";
 import {MakeTeam} from '../components/index';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {RootState} from '../redux-modules/index';
+import {setTeamInfo} from '../redux-modules/makeTeam';
+
+interface Param {
+    value: string
+    label?: string
+    name: string
+}
 
 function MakeTeamContainers(){
-    const teamInfo = useSelector((state: RootState)=> state.makeTeam.teamInfo);
+    const dispatch = useDispatch();
+    const {teamLeaderInfo, teamInfo} = useSelector((state: RootState)=> state.makeTeam);
+
+    const onInputChanged = (param: Param)=>{
+        const {value, name} = param;
+        let obj = null;
+
+        if(name === 'teamName'){
+            obj = teamInfo ? {
+                ...teamInfo,
+                teamName: value
+            } : null;
+        }else if(name === 'purpose'){
+            const over = value.length > 50;
+            obj = teamInfo ? {
+                ...teamInfo,
+                purpose: over ? teamInfo.purpose : value,
+                wordCount: over ? 50 : value.length
+            } : null;
+        }
+        dispatch(setTeamInfo(obj));
+    }
+
+    const onComboboxSelected = (param: Param)=>{
+        const {label, name} = param;
+        const obj = teamInfo ? {
+            ...teamInfo,
+            [name]: label
+        } : null;
+        dispatch(setTeamInfo(obj));
+    }
 
     return (
-        <MakeTeam></MakeTeam>
+        <MakeTeam 
+            teamLeaderInfo={teamLeaderInfo}
+            teamInfo={teamInfo}
+            onInputChanged={onInputChanged}
+            onComboboxSelected={onComboboxSelected}/>
     );
 }
 
--- src/redux-modules/makeTeam.ts
+++ src/redux-modules/makeTeam.ts
@@ -1,21 +1,43 @@
-import {TeamInfo} from '../util/interfaces';
+import {TeamInfo, TeamLeaderInfo} from '../util/interfaces';
 
 const SET_TEAM_INFO = 'makeTeam/SET_TEAM_INFO' as const;
+const SET_TEAMLEADER_INFO = 'makeTeam/SET_TEAMLEADER_INFO' as const;
 
 export const setTeamInfo = (arg: TeamInfo | null)=> ({ 
     type : SET_TEAM_INFO,
     payload : arg
 });
+export const setTeamLeaderInfo = (arg: TeamLeaderInfo)=> ({ 
+    type : SET_TEAMLEADER_INFO,
+    payload : arg
+});
 
 type actionType = 
     | ReturnType<typeof setTeamInfo>
+    | ReturnType<typeof setTeamLeaderInfo>
 
 type stateType = {
     teamInfo: TeamInfo | null
+    teamLeaderInfo: TeamLeaderInfo
 }
 
 const initState: stateType = {
-    teamInfo: null
+    teamInfo: {
+        teamName: '',
+        category: '',
+        purpose: '',
+        filter_1: '',
+        filter_2: '',
+        filter_3: '',
+        career: '',
+        wordCount: 0,
+    },
+    teamLeaderInfo: {
+        career: '',
+        nickName: '',
+        gender: '',
+        propensity: '',
+    }
 };
 
 function makeTeam(state: stateType = initState, action: actionType) {
@@ -26,6 +48,13 @@ function makeTeam(state: stateType = initState, action: actionType) {
                 teamInfo: action.payload ? {
                     ...action.payload
                 } : null
+            };
+        case SET_TEAMLEADER_INFO:
+            return {
+                ...state,
+                teamLeaderInfo: {
+                    ...action.payload
+                }
             };
         default:
             return state;
